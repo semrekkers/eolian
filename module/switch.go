6db8e3daad89feb688fe876bb15457f721d79117
Brett Buddin <brett@buddin.us>
2016-11-19 11:33:05 -0500

    Change name of input to clock.

--- module/switch.go
+++ module/switch.go
@@ -23,20 +23,20 @@ func init() {
 
 type Switch struct {
 	IO
-	trigger, reset *In
-	sources        []*In
+	clock, reset *In
+	sources      []*In
 
-	step                   int
-	lastTrigger, lastReset Value
+	step                 int
+	lastClock, lastReset Value
 }
 
 func NewSwitch(size int) (*Switch, error) {
 	m := &Switch{
-		trigger:     &In{Name: "trigger", Source: NewBuffer(zero)},
-		reset:       &In{Name: "reset", Source: NewBuffer(zero)},
-		lastTrigger: -1,
+		clock:     &In{Name: "clock", Source: NewBuffer(zero)},
+		reset:     &In{Name: "reset", Source: NewBuffer(zero)},
+		lastClock: -1,
 	}
-	inputs := []*In{m.trigger, m.reset}
+	inputs := []*In{m.clock, m.reset}
 	for i := 0; i < size; i++ {
 		in := &In{
 			Name:   fmt.Sprintf("%d.input", i),
@@ -50,18 +50,18 @@ func NewSwitch(size int) (*Switch, error) {
 }
 
 func (reader *Switch) Read(out Frame) {
-	trigger := reader.trigger.ReadFrame()
-	reset := reader.trigger.ReadFrame()
+	clock := reader.clock.ReadFrame()
+	reset := reader.reset.ReadFrame()
 	for i := 0; i < len(reader.sources); i++ {
 		reader.sources[i].ReadFrame()
 	}
 	for i := range out {
 		if reader.lastReset < 0 && reset[i] > 0 {
 			reader.step = 0
-		} else if reader.lastTrigger < 0 && trigger[i] > 0 {
+		} else if reader.lastClock < 0 && clock[i] > 0 {
 			reader.step = (reader.step + 1) % len(reader.sources)
 		}
 		out[i] = reader.sources[reader.step].LastFrame()[i]
-		reader.lastTrigger = trigger[i]
+		reader.lastClock = clock[i]
 	}
 }
