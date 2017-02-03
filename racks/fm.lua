18db582f6a7f5f39a923543d96a96064f86554d6
Brett Buddin <brett@buddin.us>
2017-02-03 17:59:20 -0500

    Clocks and fm

--- racks/fm.lua
+++ racks/fm.lua
@@ -4,28 +4,55 @@ return function(env)
 
     local function build()
         return {
+            clock = {
+                osc      = interp(synth.Oscillator(), { pitch = { min = hz(1), max = hz(20) }}),
+                multiple = synth.Multiple(),
+            },
+
             midi   = synth.MIDIController { device = 'Launch Control' },
             osc1   = interp(synth.Oscillator(), { pitch = { min = hz(1), max = hz(100) } }),
             osc2   = interp(synth.Oscillator(), { pitch = { min = hz(100), max = hz(2000) } }),
             filter = interp(synth.Filter(), { cutoff = { max = hz(5000) }, resonance = { max = 100 } }),
             mix    = synth.Mix(),
+
+            adsr   = interp(synth.ADSR(), {
+                attack  = { min = ms(1), max = ms(500) },
+                decay   = { min = ms(1), max = ms(500) },
+                release = { min = ms(1), max = ms(500) },
+            }),
+            amp    = synth.Multiply(),
         }
     end
 
     local function patch(m)
-        local ch1 = m.midi:ns('cc/1')
+        local cc = function(n) return out(m.midi:ns('cc/1'), n) end
 
-        set(m.osc1, { pitch = out(ch1, 21) })
-        set(m.osc2, { pitch = out(ch1, 23), pitchModAmount = out(ch1, 22), pitchMod = out(m.osc1, 'saw') })
-        set(m.filter, { input = out(m.osc2, 'sine'), cutoff = out(ch1, 41), resonance = out(ch1, 42) })
+        local clock = with(m.clock, function(c)
+            set(c.osc, { pitch = cc(28), pulseWidth = cc(27) })
+            set(c.multiple, { input = out(c.osc, 'pulse') })
+            return c.multiple
+        end)
 
-        set(m.mix, {
-            { input = out(m.filter, 'lowpass'), level = out(ch1, 43) },
-            { input = out(m.filter, 'bandpass'), level = out(ch1, 44) },
-            { input = out(m.filter, 'highpass'), level = out(ch1, 45) },
+        set(m.adsr, {
+            gate    = out(clock, 0),
+            attack  = cc(45),
+            decay   = cc(46),
+            sustain = cc(47),
+            release = cc(48),
         })
 
-        return out(m.mix)
+        set(m.osc1, { pitch = cc(21) })
+        set(m.osc2, { pitch = cc(23), pitchModAmount = cc(22), pitchMod = out(m.osc1, 'saw') })
+        set(m.filter, { input = out(m.osc2, 'sine'), cutoff = cc(41), resonance = cc(42) })
+
+        set(m.mix, {
+            { input = out(m.filter, 'lowpass'), level = cc(43) },
+            { input = out(m.filter, 'bandpass'), level = cc(44) },
+            -- { input = out(m.filter, 'highpass'), level = cc(45) },
+        })
+
+        set(m.amp, { a = out(m.mix), b = out(m.adsr) })
+        return out(m.amp)
     end
 
     return build, patch
