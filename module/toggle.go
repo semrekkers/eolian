8ee4ef3bdd57941d1879e06b968bd297d0d5adce
Brett Buddin <brett@buddin.us>
2017-02-18 12:55:57 -0500

    Be a bit more loose with what Toggle expects in a trigger.

--- module/toggle.go
+++ module/toggle.go
@@ -12,7 +12,8 @@ type toggle struct {
 
 func newToggle() (*toggle, error) {
 	m := &toggle{
-		trigger: &In{Name: "trigger", Source: NewBuffer(zero)},
+		trigger:     &In{Name: "trigger", Source: NewBuffer(zero)},
+		lastTrigger: -1,
 	}
 	return m, m.Expose(
 		"Toggle",
@@ -24,7 +25,7 @@ func newToggle() (*toggle, error) {
 func (t *toggle) Read(out Frame) {
 	trigger := t.trigger.ReadFrame()
 	for i := range out {
-		if t.lastTrigger < 0 && trigger[i] > 0 {
+		if t.lastTrigger <= 0 && trigger[i] > 0 {
 			if t.value == 1 {
 				t.value = -1
 			} else {
