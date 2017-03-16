d2bf5b9c2b9b45a490d333814097f466f54cbef7
Brett Buddin <brett@buddin.us>
2017-03-16 13:58:33 -0400

    Move gops to main.go

--- cmd/eolian/main.go
+++ cmd/eolian/main.go
@@ -5,9 +5,13 @@ import (
 	"os"
 
 	"github.com/brettbuddin/eolian/command"
+	"github.com/google/gops/agent"
 )
 
 func main() {
+	if err := agent.Listen(nil); err != nil {
+		log.Fatal(err)
+	}
 	if err := command.Run(os.Args[1:]); err != nil {
 		log.Println(err)
 	}
