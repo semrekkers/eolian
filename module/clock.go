f15562fb6d864545be3d55a046fba59c99acffa0
Brett Buddin <brett@buddin.us>
2017-03-20 21:51:04 -0400

    Just pass divide-by-1 through. It'll never reset.

--- module/clock.go
+++ module/clock.go
@@ -225,7 +225,8 @@ func (d *rcd) Read(out Frame) {
 				d.rotation = 0
 			}
 
-			for j := 0; j < len(d.outs); j++ {
+			d.outs[0][i] = in[i]
+			for j := 1; j < len(d.outs); j++ {
 				count := j - d.rotation
 				if count < 0 {
 					count = d.maxRotation + count
