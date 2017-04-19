c6475e2de7e9fccd56c1cca442bc20b410465f29
Brett Buddin <brett@buddin.us>
2017-04-19 15:45:40 -0400

    Master gate output for StepSequence and modes like StageSequence.

--- module/step_sequencer.go
+++ module/step_sequencer.go
@@ -2,6 +2,7 @@ package module
 
 import (
 	"fmt"
+	"math/rand"
 
 	"buddin.us/eolian/dsp"
 
@@ -33,24 +34,28 @@ func init() {
 type stepSequence struct {
 	multiOutIO
 
-	clock, reset                          *In
+	clock, reset, mode                    *In
 	enables                               []*In
 	pitches                               [][]*In
 	step, lastStep, layerCount, stepCount int
+	pong                                  bool
 
 	lastClock, lastReset dsp.Float64
 
 	pitchesOut, gatesOut []dsp.Frame
+	allGateOut           dsp.Frame
 }
 
 func newStepSequence(steps, layers int) (*stepSequence, error) {
 	m := &stepSequence{
 		clock:      NewInBuffer("clock", dsp.Float64(0)),
 		reset:      NewInBuffer("reset", dsp.Float64(0)),
-		pitchesOut: make([]dsp.Frame, layers),
+		mode:       NewInBuffer("mode", dsp.Float64(0)),
 		pitches:    make([][]*In, layers),
 		enables:    make([]*In, steps),
 		gatesOut:   make([]dsp.Frame, steps),
+		pitchesOut: make([]dsp.Frame, layers),
+		allGateOut: dsp.NewFrame(),
 		lastClock:  -1,
 		lastReset:  -1,
 		lastStep:   -1,
@@ -59,16 +64,19 @@ func newStepSequence(steps, layers int) (*stepSequence, error) {
 	}
 
 	var (
-		inputs  = []*In{m.clock, m.reset}
-		outputs = []*Out{}
+		inputs  = []*In{m.clock, m.reset, m.mode}
+		outputs = []*Out{&Out{Name: "gate", Provider: provideCopyOut(m, &m.allGateOut)}}
 	)
 
 	for i := 0; i < layers; i++ {
+		// Layer Steps
 		m.pitches[i] = make([]*In, steps)
 		for j := 0; j < steps; j++ {
 			m.pitches[i][j] = NewInBuffer(fmt.Sprintf("%c/%d/pitch", alphaSeries[i], j), dsp.Float64(0))
 			inputs = append(inputs, m.pitches[i][j])
 		}
+
+		// Layer Pitch
 		m.pitchesOut[i] = dsp.NewFrame()
 		outputs = append(outputs, &Out{
 			Name:     fmt.Sprintf("%c/pitch", alphaSeries[i]),
@@ -80,6 +88,7 @@ func newStepSequence(steps, layers int) (*stepSequence, error) {
 		m.enables[i] = NewInBuffer(fmt.Sprintf("%d/enabled", i), dsp.Float64(1))
 		inputs = append(inputs, m.enables[i])
 
+		// Step Gate
 		m.gatesOut[i] = dsp.NewFrame()
 		outputs = append(outputs, &Out{
 			Name:     fmt.Sprintf("%d/gate", i),
@@ -94,6 +103,7 @@ func (s *stepSequence) Process(out dsp.Frame) {
 	s.incrRead(func() {
 		clock := s.clock.ProcessFrame()
 		reset := s.reset.ProcessFrame()
+		mode := s.mode.ProcessFrame()
 		for l, layer := range s.pitches {
 			for i := range layer {
 				s.pitches[l][i].ProcessFrame()
@@ -105,8 +115,29 @@ func (s *stepSequence) Process(out dsp.Frame) {
 
 		for i := range out {
 			if s.lastStep >= 0 && s.lastClock < 0 && clock[i] > 0 {
-				s.step = (s.step + 1) % s.stepCount
+				switch mapPatternMode(mode[i]) {
+				case patternModeSequential:
+					s.step = (s.step + 1) % s.stepCount
+					s.pong = false
+				case patternModePingPong:
+					if s.pong {
+						s.step -= 1
+					} else {
+						s.step += 1
+					}
+					if s.step == s.stepCount-1 {
+						s.step = s.stepCount - 1
+						s.pong = true
+					} else if s.step == 0 {
+						s.step = 0
+						s.pong = false
+					}
+				case patternModeRandom:
+					s.step = rand.Intn(s.stepCount)
+					s.pong = false
+				}
 			}
+
 			if s.lastReset < 0 && reset[i] > 0 {
 				s.step = 0
 			}
@@ -137,5 +168,6 @@ func (s *stepSequence) fillGates(i int, clock dsp.Float64) {
 		} else {
 			s.gatesOut[j][i] = -1
 		}
+		s.allGateOut[i] = dsp.Max(s.allGateOut[i], s.gatesOut[j][i])
 	}
 }
